bb575ea3e4bb32a66ebc0c173bb8695b8f03d8ce
Sagar <>
2024-10-04 19:12:24 +0530

    Day 123:Best Time to Buy and sell Stock

---
+++ Day 123/bestTimeToBuyAndSellStock.java
@@ -0,0 +1,29 @@
+/**
+ * bestTimeToBuyAndSellStock
+ */
+public class bestTimeToBuyAndSellStock {
+    public static int maxProfit(int []prices){
+        if(prices==null||prices.length==0){
+            return 0;
+        }
+        int minPrice=prices[0];
+        int maxProfit=0;
+        for (int i = 1; i < prices.length; i++) {
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }
+            else{
+                int profit=prices[i]-minPrice;
+                if(profit>maxProfit){
+                    maxProfit=profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}

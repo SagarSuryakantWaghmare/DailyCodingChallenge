6908b86e636a85b62220785f13298d8365d7390c
Sagar <sagarwaghmare1384@gmail.com>
2024-10-22 18:53:27 +0530

    Another solution for the power of two

--- Day 148/powerOfTwo.java
+++ Day 148/powerOfTwo.java
@@ -2,15 +2,27 @@
  * powerOfTwo
  */
 public class powerOfTwo {
+    // O(1)
+    // public static boolean isPowerOfTwo(int n){
+    //     if(n==0) return true;
+    //     for(int i=1;i<31;i++){
+    //         int result=(int)i*i;
+    //         if(result==n){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // O(logn)
     public static boolean isPowerOfTwo(int n){
-        if(n==0) return true;
-        for(int i=1;i<31;i++){
-            int result=(int)i*i;
-            if(result==n){
-                return true;
-            }
+        if(n<=0){
+            return false;
         }
-        return false;
+        while (n%2==0) {
+            n/=2;
+        }
+        return n==1;
     }
 
     public static void main(String[] args) {

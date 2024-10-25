7a53f87c1bd3bdf8e68902bf9469a1019999da1b
Sagar <sagarwaghmare1384@gmail.com>
2024-10-25 18:19:34 +0530

    Day 156:Check Array Sort

---
+++ Day 156/CheckArraySort.java
@@ -0,0 +1,18 @@
+/**
+ * CheckArraySort
+ */
+public class CheckArraySort {
+    public static boolean isSorted(int []arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(isSorted(arr));
+    }
+}

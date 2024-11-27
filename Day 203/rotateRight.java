44cd4dfcd6cabb05cd6396908d09d4a98d064db5
Sagar <sagarwaghmare1384@gmail.com>
2024-11-27 16:48:33 +0530

    Day 203:rotate Right

---
+++ Day 203/rotateRight.java
@@ -0,0 +1,53 @@
+
+public class rotateRight {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            ListNode current = this;
+            while (current != null) {
+                sb.append(current.val);
+                if (current.next != null) {
+                    sb.append(" -> ");
+                }
+                current = current.next;
+            }
+            return sb.toString();
+        }
+    }
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode temp = head;
+        int length = 1;
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+        temp.next = head;
+        k = k % length;
+        int skip = length - k;
+        temp = head;
+        for (int i = 0; i < skip - 1; i++) {
+            temp = temp.next;
+        }
+        head = temp.next;
+        temp.next = null;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println(rotateRight(head, 2));
+    }
+}

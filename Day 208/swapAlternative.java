057e63fe1f5d66ec933e171a5454e7455a962e2e
Sagar <sagarwaghmare1384@gmail.com>
2024-12-01 15:49:16 +0530

    Day 208 :Swap alternative

--- Day 208/swapAlternative.java
+++ Day 208/swapAlternative.java
@@ -1,3 +1,18 @@
 public class swapAlternative {
-    
+    public static void main(String[] args) {
+        char[]a={'t','i','g','e','r'};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Swap alternative char:");
+        for (int i = 0; i < a.length-1; i+=2) {
+            char temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+    }
 }

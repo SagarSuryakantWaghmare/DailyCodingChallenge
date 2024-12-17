6553745942dce35e5fbbeb82321722c3cd2ccfc0
Sagar <sagarwaghmare1384@gmail.com>
2024-12-17 13:36:14 +0530

    Day 224:Minimum Depth

---
+++ Day 224/MinimumDepth.java
@@ -0,0 +1,33 @@
+public class MinimumDepth {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static int minDepth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        if(root.left==null){
+            return 1+minDepth(root.right);
+        }
+        if(root.right==null){
+            return 1+minDepth(root.left);
+        }
+        return 1+Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
+        System.out.println("Minimum Depth:"+minDepth(root));
+    }
+}

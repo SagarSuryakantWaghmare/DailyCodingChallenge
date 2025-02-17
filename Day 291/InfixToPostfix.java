8ffd5749744ab59c262a89ff12806b5ab7741277
Sagar <sagarwaghmare1384@gmail.com>
2025-02-17 14:34:13 +0530

    infix to postfix

---
+++ Day 291/InfixToPostfix.java
@@ -0,0 +1,50 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+    public static String postfix(String s) {
+        StringBuilder ans = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                ans.append(ch);
+            } else if (ch == '(') {
+                st.push(ch);
+            } else if (ch == ')') {
+                while (!st.isEmpty() && st.peek() != '(') {
+                    ans.append(st.pop());
+                }
+                st.pop(); // Pop '('
+            } else {
+                while (!st.isEmpty() && priority(ch) <= priority(st.peek())) {
+                    ans.append(st.pop());
+                }
+                st.push(ch);
+            }
+        }
+
+        // Pop remaining operators from stack
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
+        }
+
+        return ans.toString();
+    }
+
+    public static int priority(char ch) {
+        if (ch == '+' || ch == '-')
+            return 1;
+        if (ch == '*' || ch == '/')
+            return 2;
+        if (ch == '^')
+            return 3;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "a+b*(c^d-c)";
+        System.out.println("Postfix: " + postfix(s)); // Expected: "abcd^c-*+"
+    }
+}
